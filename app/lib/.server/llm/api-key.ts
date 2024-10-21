e3e5af594bbeb453f2e175891ab13360895585ae
Manish Gupta <59428681+mguptahub@users.noreply.github.com>
2024-10-21 05:30:25 +0000

    updated packages, added openai

--- app/lib/.server/llm/api-key.ts
+++ app/lib/.server/llm/api-key.ts
@@ -5,5 +5,12 @@ export function getAPIKey(cloudflareEnv: Env) {
    * The `cloudflareEnv` is only used when deployed or when previewing locally.
    * In development the environment variables are available through `env`.
    */
-  return env.ANTHROPIC_API_KEY || cloudflareEnv.ANTHROPIC_API_KEY;
+  let apiKey=""
+  if ( env.AI_MODEL_PROVIDER == "openai"){
+    apiKey = (env.OPENAI_API_KEY || cloudflareEnv.OPENAI_API_KEY);
+  }else{
+    apiKey = (env.ANTHROPIC_API_KEY || cloudflareEnv.ANTHROPIC_API_KEY);
+  }
+  return apiKey;
+
 }
--- app/lib/.server/llm/model.ts
+++ app/lib/.server/llm/model.ts
@@ -1,9 +1,31 @@
+import { env } from 'node:process';
 import { createAnthropic } from '@ai-sdk/anthropic';
+import { createOpenAI } from '@ai-sdk/openai';
+import type { LanguageModel } from 'ai';
 
-export function getAnthropicModel(apiKey: string) {
+function getAnthropicModel(apiKey: string) {
   const anthropic = createAnthropic({
     apiKey,
+    headers: {
+      'anthropic-beta': 'max-tokens-3-5-sonnet-2024-07-15',
+    },
   });
 
   return anthropic('claude-3-5-sonnet-20240620');
+}
+
+function getOpenAIModel(apiKey: string) {
+  const openai = createOpenAI({
+    apiKey,
+    compatibility: 'strict',
+  });
+  return openai('gpt-4o');
+}
+
+export function getAIModel(apiKey: string) {
+  if (env.AI_MODEL_PROVIDER == "openai"){
+    return getOpenAIModel(apiKey)
+  }else{
+    return getAnthropicModel(apiKey)
+  }
 }
